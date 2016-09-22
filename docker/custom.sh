6f29f53b9933b2607759555f4cfe69d8bb1c5df1
Marko Heijnen <info@markoheijnen.com>
2016-09-22 19:13:45 +0200

    Fix search-replace for this theme

--- docker/custom.sh
+++ docker/custom.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-
-wp search-replace http://local.wordpress.dev "$WORDPRESS_URL" --recurse-objects --allow-root
+DOMAIN_NO_PROTOCOL=$(echo "$WORDPRESS_URL" | sed 's~http[s]*://~~g')
+wp search-replace local.wordpress.dev "$DOMAIN_NO_PROTOCOL" --recurse-objects --allow-root
