c52d3a07abdbbc5dc060e7675b1cec3529505c98
Marko Heijnen <info@markoheijnen.com>
2016-09-28 10:37:56 +0200

    Fix static tag value. See #9

--- manage_wordpress.sh
+++ manage_wordpress.sh
@@ -385,7 +385,7 @@ if [[ $ACTION == "create" ]]; then
    	# ----- CREATE ELB -----
    	STEP=$((STEP+1))
     echo "[$STEP/$STEPS] Check Elastic Loadbalancer..."
-    OUTPUT=$(aws elb describe-load-balancers --load-balancer-names=pleskwp)
+    OUTPUT=$(aws elb describe-load-balancers --load-balancer-names=$TAG)
     ELB=$(search_value "$OUTPUT" "DNSName" "LoadBalancerName" "$ELB_NAME")
     ELB_ID=$(search_value "$OUTPUT" "CanonicalHostedZoneNameID")
     if [[ -z $ELB ]]; then
