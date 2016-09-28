466610b79b301e4034f9573328bcf5237a2ceceb
Marko Heijnen <info@markoheijnen.com>
2016-09-28 16:37:48 +0200

    Only do search-replace if domain is not empty

--- docker/custom.sh
+++ docker/custom.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+
 DOMAIN_NO_PROTOCOL=$(echo "$WORDPRESS_URL" | sed 's~http[s]*://~~g')
-wp search-replace local.wordpress.dev "$DOMAIN_NO_PROTOCOL" --recurse-objects --allow-root
+if [[ -z $DOMAIN_NO_PROTOCOL ]]; then
+	wp search-replace local.wordpress.dev "$DOMAIN_NO_PROTOCOL" --recurse-objects --allow-root
+fi
