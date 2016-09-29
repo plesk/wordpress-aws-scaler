313b6ab15b2f12e8d483f405760c2eb913ad13d7
Marko Heijnen <info@markoheijnen.com>
2016-09-29 09:27:46 +0200

    Fix search-replace in custom.sh

--- docker/custom.sh
+++ docker/custom.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-
+echo "hello $WORDPRESS_URL"
 DOMAIN_NO_PROTOCOL=$(echo "$WORDPRESS_URL" | sed 's~http[s]*://~~g')
-if [[ -z $DOMAIN_NO_PROTOCOL ]]; then
+echo "$DOMAIN_NO_PROTOCOL"
+
+wp search-replace http://local.wordpress.dev "$WORDPRESS_URL" --recurse-objects --allow-root
+if [[ -n $DOMAIN_NO_PROTOCOL ]]; then
 	wp search-replace local.wordpress.dev "$DOMAIN_NO_PROTOCOL" --recurse-objects --allow-root
 fi
