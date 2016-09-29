091c4e78f240bdd4f00a817bfd6a5ce00b18e189
Marko Heijnen <info@markoheijnen.com>
2016-09-29 17:43:33 +0200

    Remove debug  code

--- docker/custom.sh
+++ docker/custom.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-echo "hello $WORDPRESS_URL"
 DOMAIN_NO_PROTOCOL=$(echo "$WORDPRESS_URL" | sed 's~http[s]*://~~g')
-echo "$DOMAIN_NO_PROTOCOL"
-
 wp search-replace http://local.wordpress.dev "$WORDPRESS_URL" --recurse-objects --allow-root
 if [[ -n $DOMAIN_NO_PROTOCOL ]]; then
 	wp search-replace local.wordpress.dev "$DOMAIN_NO_PROTOCOL" --recurse-objects --allow-root
